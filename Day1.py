88246b446f504683258063e07228dc80c6bb0311
Sanjay Kumar <46316162+MrUnKnown786@users.noreply.github.com>
2021-07-29 09:49:27 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
 print("Take input for a list and sort it in descending order")
-list1 = list(map(int, input("Enter Numbers with Space : \n").split()))
+list1 = list(map(int, input("Enter list of numbers with space :\n").split()))
 list1.sort(reverse=True)
 print(list1)
